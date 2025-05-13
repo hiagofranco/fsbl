55044a8b26c35eff7486251862b874dbcb74ebb6
Hiago De Franco <hiagofranco@gmail.com>
2025-05-12 21:28:42 -0300

    plat: cv181x: bl2: enable uart
    Current UART needs proper initialization to work. Enable it so it is
    possible to see FSBL messages.

--- plat/cv181x/bl2/bl2_main.c
+++ plat/cv181x/bl2/bl2_main.c
@@ -47,8 +47,30 @@ int dec_verify_image(const void *image, size_t size, size_t dec_skip, struct fip
 }
 #endif
 
+#define UART_DLL 0x04140000
+#define UART_DLH 0x04140004
+#define UART_LCR 0x0414000C
+
+void set_baudrate()
+{
+	// 14 for 115200, 13 for 128000
+	int baud_divisor = 14;
+
+	// set DLAB to 1 to set dll and dlh
+	*(volatile uint32_t*)(UART_LCR) |= (uint32_t)0x80;
+
+	// set divisor
+	*(volatile uint32_t*)(UART_DLL) = (uint32_t)(baud_divisor & 0xff);
+	*(volatile uint32_t*)(UART_DLH) = (uint32_t)((baud_divisor >> 8) & 0xff);
+
+	// set DLAB back to 0
+	*(volatile uint32_t*)(UART_LCR) &= (uint32_t)(~0x80);
+}
+
 void bl2_main(void)
 {
+	set_baudrate();
+
 	uint32_t v = p_rom_api_get_boot_src();
 
 	if (v == BOOT_SRC_UART) {
